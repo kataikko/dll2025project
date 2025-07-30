29003a4f1fc952272450c56517c67580230037c3
Vincent Kataikko <vincent@kataikko.de>
2025-07-30 12:39:22 +0200

    feat: added shape logging

--- common3d/src/od3d/cv/geometry/objects3d/dmtet_x_gaussians/dmtet_x_gaussians.py
+++ common3d/src/od3d/cv/geometry/objects3d/dmtet_x_gaussians/dmtet_x_gaussians.py
@@ -271,7 +271,6 @@ class DMTet_x_Gaussians(Meshes_x_Gaussians):
                     )
 
             if require_feats_grad:
-                # logger.info(m)
                 # logger.info(_verts.shape)
                 _feats = self.get_feats(pts=_verts.detach(), object_id=m)
             else:
@@ -281,6 +280,11 @@ class DMTet_x_Gaussians(Meshes_x_Gaussians):
             verts.append(_verts)
             faces.append(_faces)
             feats.append(_feats)
+
+            # TODO: Remove logging
+            logger.info(f"VLOG - _verts: {_verts.shape}")
+            logger.info(f"VLOG - _faces: {_faces.shape}")
+            logger.info(f"VLOG - _feats: {_feats.shape}")
 
         factory_kwargs = {"device": device, "dtype": dtype}
 
@@ -339,6 +343,13 @@ class DMTet_x_Gaussians(Meshes_x_Gaussians):
         self.feats_rgb_object_id = []
         for i in range(len(self)):
             self.feats_rgb_object_id.extend([color_(i)] * self.verts_counts[i])
+
+        # TODO: Remove logging
+        logger.info(f"VLOG - self.feat_clutter: {self.feat_clutter.shape}")
+        logger.info(f"VLOG - self.verts: {self.verts.shape}")
+        logger.info(f"VLOG - self.feats_objects: {self.feats_objects.shape}")
+        logger.info(f"VLOG - self.faces: {self.faces.shape}")
+        logger.info(f"VLOG - self.mask_verts_not_padded: {self.mask_verts_not_padded.shape}")
 
         self.update_verts_coarse()
         #### UPDATE MESHES x Gaussians
--- common3d/src/od3d/cv/geometry/objects3d/flexicubes/flexicubes.py
+++ common3d/src/od3d/cv/geometry/objects3d/flexicubes/flexicubes.py
@@ -1,3 +1,7 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
 import torch
 
 from omegaconf import DictConfig
@@ -284,6 +288,11 @@ class Flexicubes(Meshes):
             faces.append(_faces)
             feats.append(_feats)
 
+            # TODO: Remove logging
+            logger.info(f"VLOG - _verts: {_verts.shape}")
+            logger.info(f"VLOG - _faces: {_faces.shape}")
+            logger.info(f"VLOG - _feats: {_feats.shape}")
+
         factory_kwargs = {"device": device, "dtype": dtype}
 
         self.verts_counts = [_verts.shape[0] for _verts in verts]
@@ -341,7 +350,13 @@ class Flexicubes(Meshes):
         for i in range(len(self)):
             self.feats_rgb_object_id.extend([color_(i)] * self.verts_counts[i])
 
-        # TODO: Maybe this is DMTet specific, we can remove? 
+        # TODO: Remove logging
+        logger.info(f"VLOG - self.feat_clutter: {self.feat_clutter.shape}")
+        logger.info(f"VLOG - self.verts: {self.verts.shape}")
+        logger.info(f"VLOG - self.feats_objects: {self.feats_objects.shape}")
+        logger.info(f"VLOG - self.faces: {self.faces.shape}")
+        logger.info(f"VLOG - self.mask_verts_not_padded: {self.mask_verts_not_padded.shape}")
+
         self.update_verts_coarse()
 
     def get_sdf(self, pts, object_id):
